2cf02aac2f7b78925f94158ecfa93d650144e0dd
gokarna123-goku <agokarna444@gmail.com>
2023-09-21 12:50:04 +0545

    changes the things

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,9 +4,12 @@ import "./Home.css";
 const Home = () => {
   return (
     <>
-      <div className="hero_img">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit id at enim
-        harum tenetur perferendis blanditiis laboriosam sunt quisquam odio?
+      <div className="hero_img px-32">
+        <div className="w-2/5">
+          <h1 className="text-slate-100 text-7xl font-black leading-[5.2rem] tracking-wider">
+            Gokarna Bdr. Adhikari
+          </h1>
+        </div>
       </div>
     </>
   );
