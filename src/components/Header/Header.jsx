da4b60ea6ad2c30abace4f0f7d409ca0bdd2f680
gokarna123-goku <agokarna444@gmail.com>
2023-09-21 15:40:02 +0545

    working on the logo section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <nav className="w-full bg-[#1f1f38] py-5 px-32">
-        Hello
+    <nav className="w-full bg-[#131322] py-5 px-32 shadow-xl">
+        .
     </nav>
   )
 }
