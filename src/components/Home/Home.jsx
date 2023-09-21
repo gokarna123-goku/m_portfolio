887f8d3b38f5178b35f3f6300a25940ddb0b11f9
gokarna123-goku <agokarna444@gmail.com>
2023-09-21 15:25:08 +0545

    completed the hero section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,21 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Home.css";
+import { ArrowUpRight } from "react-bootstrap-icons";
 
 const Home = () => {
   return (
     <>
       <div className="hero_img px-32">
         <div className="w-2/4">
-          <h6 className="text-slate-400 mb-2 text-xl">Hey 👋 There, It's</h6>
-          <h1 className="text-slate-100 text-8xl font-black leading-[7.3rem] tracking-[0.12rem] mb-3">
+          {/* <h6 className="text-slate-400 mb-2 text-xl">Hey 👋 There, It's</h6> */}
+          <p className="text-slate-500  text-2xl font-medium tracking-[0.05rem] mb-4">
+            Meet a Front~End Developer💻,
+          </p>
+          <h1 className="text-slate-100 text-8xl font-black leading-[7.3rem] tracking-[0.12rem] mb-8">
             Mr. <span className="text-green-400">Gokarna</span> Adhikari!
           </h1>
-          <p className="text-slate-500 uppercase text-3xl font-medium tracking-[0.1rem] mb-8">
-            A Front-End Developer💻
-          </p>
-          <button type="button" className="bg-green-500 text-xl text-slate-950 font-medium px-5 py-2 rounded-md hover:bg-green-200">
-            Say Hello 
-            </button>
+          <Link className="flex items-center text-xl text-green-500 text-left font-medium px-0 py-2 rounded-md hover:text-green-400 ease-out duration-1000">
+            Say Hello <ArrowUpRight className="ms-2 text-lg" />
+          </Link>
         </div>
       </div>
     </>
