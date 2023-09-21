3e2c7ba64f5f0732742e71412e40c3110c0988f0
gokarna123-goku <agokarna444@gmail.com>
2023-09-21 15:30:23 +0545

    working on the header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
       <Router>
         <div className="main_section w-full min-h-screen">
+          <Header />
           <Home />
         </div>
       </Router>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <nav className="w-full">
-        
+    <nav className="w-full bg-[#1f1f38] py-5 px-32">
+        Hello
     </nav>
   )
 }
