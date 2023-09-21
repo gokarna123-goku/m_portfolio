1356ccf5ad1b249f491fa9c785e1987b74dc7777
gokarna123-goku <agokarna444@gmail.com>
2023-09-21 12:35:28 +0545

    added the bg images

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,7 +4,7 @@ import "./Home.css";
 const Home = () => {
   return (
     <>
-      <div className="w-full hero_img">
+      <div className="hero_img">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit id at enim
         harum tenetur perferendis blanditiis laboriosam sunt quisquam odio?
       </div>
