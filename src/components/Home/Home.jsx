90c8a95caf4d2f01dc85299a6816b3cd9187497b
gokarna123-goku <agokarna444@gmail.com>
2023-09-21 13:26:11 +0545

    almost completed the hero section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,11 +5,14 @@ const Home = () => {
   return (
     <>
       <div className="hero_img px-32">
-        <div className="w-2/5">
-          <h6 className="text-slate-400 mb-2 text-xl">Hey 👋 There, I'm</h6>
-          <h1 className="text-slate-100 text-7xl font-black leading-[5.5rem] tracking-[0.12rem]">
-            Mr. Gokarna Adhikari!
+        <div className="w-2/4">
+          <h6 className="text-slate-400 mb-2 text-xl">Hey 👋 There, It's</h6>
+          <h1 className="text-slate-100 text-8xl font-black leading-[7.3rem] tracking-[0.12rem] mb-3">
+            Mr. <span className="text-green-500">Gokarna</span> Adhikari!
           </h1>
+          <p className="text-slate-500 uppercase text-3xl font-medium tracking-[0.1rem]">
+            A Front-End Developer💻
+          </p>
         </div>
       </div>
     </>
