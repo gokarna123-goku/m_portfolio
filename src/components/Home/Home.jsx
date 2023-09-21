f2944cf5349f7a1abe22251598c4ea41194fa009
gokarna123-goku <agokarna444@gmail.com>
2023-09-21 14:17:46 +0545

    working on the button section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,11 +8,14 @@ const Home = () => {
         <div className="w-2/4">
           <h6 className="text-slate-400 mb-2 text-xl">Hey 👋 There, It's</h6>
           <h1 className="text-slate-100 text-8xl font-black leading-[7.3rem] tracking-[0.12rem] mb-3">
-            Mr. <span className="text-green-500">Gokarna</span> Adhikari!
+            Mr. <span className="text-green-400">Gokarna</span> Adhikari!
           </h1>
-          <p className="text-slate-500 uppercase text-3xl font-medium tracking-[0.1rem]">
+          <p className="text-slate-500 uppercase text-3xl font-medium tracking-[0.1rem] mb-8">
             A Front-End Developer💻
           </p>
+          <button type="button" className="bg-green-500 text-xl text-slate-950 font-medium px-5 py-2 rounded-md hover:bg-green-200">
+            Say Hello 
+            </button>
         </div>
       </div>
     </>
