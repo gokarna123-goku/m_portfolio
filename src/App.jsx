d4b671c1f158ea1577af9541efe045864e5feb46
gokarna123-goku <agokarna444@gmail.com>
2023-09-22 07:53:17 +0545

    working on the section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
 function App() {
+  const [isFixed, setIsFixed] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= 80) {
+        setIsFixed(true);
+      } else {
+        setIsFixed(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const navbar = {
+    transition: "0.5s ease-out",
+  };
+
+  if (isFixed) {
+    navbar.position = "nav_fixed";
+    navbar.top = 0;
+  }
+
+  //
   return (
     <>
       <Router>
         <div className="main_section w-full min-h-screen">
-          <Header />
+        {!isFixed ? "" : <Header />}
+          {/* <Header /> */}
           <Home />
           <Footer />
         </div>
