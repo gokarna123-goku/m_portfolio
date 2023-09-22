0f4413fc1873556b67883d568b2d5408f020c75f
gokarna123-goku <agokarna444@gmail.com>
2023-09-22 12:19:50 +0545

    working on the section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <>
       <div className="hero_img px-32 relative">
-        <div className="animate-ping h-3 w-3 rounded-full absolute top-80 left-10 bg-sky-400 transition duration-700 ease-out delay-75"></div>
+        {/* <div className="animate-ping h-16 w-16 rounded-full absolute top-80 left-10 bg-sky-400 duration-1000 ease-out delay-1000 opacity-5"></div> */}
         <div className="w-2/4">
           {/* <h6 className="text-slate-400 mb-2 text-xl">Hey 👋 There, It's</h6> */}
           <p className="text-slate-500  text-2xl font-medium tracking-[0.05rem] mb-4">
