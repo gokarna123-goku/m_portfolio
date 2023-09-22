c20d9b77e03654e8ff286d426aa6d220dc6643d1
gokarna123-goku <agokarna444@gmail.com>
2023-09-22 21:29:32 +0545

    working on the section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import AboutImg from "../../assets/about.png";
+import { Check } from "react-bootstrap-icons";
 
 const About = () => {
   return (
@@ -17,12 +18,20 @@ const About = () => {
             <h1 className="text-slate-100 text-5xl font-extrabold tracking-wider mb-5">
               About me
             </h1>
-            <p className="text-gray-500">
+            <p className="text-gray-400 text-[1.08rem] font-light mb-6">
               I'm a Nepal based web designer & front-end developer focused on
               crafting clean & user-friendly experiences. I am passionate about
               building excellent software that imporoves the lives of those
               around me.
             </p>
+            <div className="flex items-center mb-4">
+                <div className="w-[20px] h-[20px] flex items-center justify-center bg-green-600 rounded-sm mr-2">
+                    <Check className="text-4xl text-gray-950"/>
+                </div>
+                <h6 className="text-gray-200 text-lg font-normal tracking-wide">I am good at problem solving,</h6>
+            </div>
+            <button className="bg-green-600 px-7 py-2 rounded text-lg text-gray-950 mr-10">Download CV</button>
+            <button className="bg-transparent border-2 border-green-600 px-7 py-1 rounded text-lg text-gray-300">Hire me</button>
           </div>
         </div>
       </div>
