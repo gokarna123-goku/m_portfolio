26d1869b8b03ad7405a20b75b57bd87cbdf4eb91
gokarna123-goku <agokarna444@gmail.com>
2023-09-22 09:01:33 +0545

    working on the footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,8 +4,12 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <>
-      <div className="w-full bg-slate-950">
-        <h1 className="text-green-500">This is Footer Section</h1>
+      <div className="w-full bg-slate-950 px-32 py-16">
+        <div className="flex items-center justify-center mb-6">
+            <h1 className="text-slate-400 text-7xl font-extrabold tracking-widest mx-8">Have a Project?</h1>
+            <button className="bg-indigo-900 px-4 py-2 rounded-md mx-8">Let's Talk</button>
+        </div>
+        <div className="divide-slate-600"></div>
       </div>
     </>
   );
