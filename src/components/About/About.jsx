a1d9fc89a5c6e890c30dd2a9cc7626232212aba2
gokarna123-goku <agokarna444@gmail.com>
2023-09-22 20:49:41 +0545

    changes the things

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,13 +5,16 @@ const About = () => {
   return (
     <>
       <div className="p-32 ">
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between gap-10">
           <div className="w-[45%] h-[33rem] bg-green-800/10 rounded-md">
             <img
               class="h-full w-full rounded-lg object-cover object-center rgba(34 197 94 / 0.5)"
               src={AboutImg}
               alt=""
             />
+          </div>
+          <div className="w-[45%] h-auto">
+            <h1 className="text-slate-100 text-5xl font-extrabold tracking-wider mb-5">About me</h1>
           </div>
         </div>
       </div>
