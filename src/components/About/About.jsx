26c77ed9d653bdce028fc68699108eedbb95ae69
gokarna123-goku <agokarna444@gmail.com>
2023-09-22 21:46:59 +0545

    working on the section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -15,20 +15,33 @@ const About = () => {
             />
           </div>
           <div className="w-[45%] h-auto">
-            <h1 className="text-slate-100 text-5xl font-extrabold tracking-wider mb-5">
+            <h1 className="text-slate-100 text-5xl font-extrabold tracking-wider mb-7">
               About me
             </h1>
-            <p className="text-gray-400 text-[1.08rem] font-light mb-6">
+            <p className="text-gray-400 text-[1.08rem] font-light mb-7">
               I'm a Nepal based web designer & front-end developer focused on
               crafting clean & user-friendly experiences. I am passionate about
               building excellent software that imporoves the lives of those
               around me.
             </p>
+            <h1 className="text-green-500 text-[1.2rem] font-medium mb-2">Why you hire me for your next project?</h1>
             <div className="flex items-center mb-4">
                 <div className="w-[20px] h-[20px] flex items-center justify-center bg-green-600 rounded-sm mr-2">
                     <Check className="text-4xl text-gray-950"/>
                 </div>
                 <h6 className="text-gray-200 text-lg font-normal tracking-wide">I am good at problem solving,</h6>
+            </div>
+            <div className="flex items-center mb-4">
+                <div className="w-[20px] h-[20px] flex items-center justify-center bg-green-600 rounded-sm mr-2">
+                    <Check className="text-4xl text-gray-950"/>
+                </div>
+                <h6 className="text-gray-200 text-lg font-normal tracking-wide">I have a good leader capacity,</h6>
+            </div>
+            <div className="flex items-center mb-10">
+                <div className="w-[20px] h-[20px] flex items-center justify-center bg-green-600 rounded-sm mr-2">
+                    <Check className="text-4xl text-gray-950"/>
+                </div>
+                <h6 className="text-gray-200 text-lg font-normal tracking-wide">I have over 1 years of experience</h6>
             </div>
             <button className="bg-green-600 px-7 py-2 rounded text-lg text-gray-950 mr-10">Download CV</button>
             <button className="bg-transparent border-2 border-green-600 px-7 py-1 rounded text-lg text-gray-300">Hire me</button>
