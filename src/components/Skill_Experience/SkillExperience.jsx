e3edd1f0a75083ff10f5ffe689824b579575a6b0
gokarna123-goku <agokarna444@gmail.com>
2023-09-23 17:17:43 +0545

    almost completed the skill section

--- src/components/Skill_Experience/SkillExperience.jsx
+++ src/components/Skill_Experience/SkillExperience.jsx
@@ -10,17 +10,37 @@ const SkillExperience = () => {
           </h1>
           <div className="flex items-start gap-10">
             <div className="skill-box w-[65%] h-auto py-2">
-              <div className="flex items-start gap-16">
-                <div className="w-1/2 bg-green-900/20 shadow px-3 py-4 rounded-md">
+              <div className="flex items-start gap-12">
+                <div className="w-1/2 bg-green-900/20 shadow p-4 rounded-md">
                   <h1 className="text-xl font-normal text-gray-400 mb-3">
                     Technological Skills
                   </h1>
-                  <div className="flex items-center justify-between pr-2 mb-1">
-                    <h6 className="text-base text-gray-300">HTML</h6>
-                    <p className="text-lg text-gray-400">90%</p>
+                  <div className="mb-4">
+                    <div className="flex items-center justify-between pr-2 mb-1">
+                      <h6 className="text-base text-gray-300">ReactJs</h6>
+                      <p className="text-lg text-gray-400">85%</p>
+                    </div>
+                    <div className="progress w-full h-auto bg-slate-400 rounded-full">
+                      <div className="indicator bg-green-600 w-[85%] h-[0.35rem] rounded-l-full"></div>
+                    </div>
                   </div>
-                  <div className="progress w-full h-auto bg-slate-400 rounded-full">
-                    <div className="indicator bg-green-600 w-[90%] h-[0.35rem] rounded-l-full"></div>
+                  <div className="mb-4">
+                    <div className="flex items-center justify-between pr-2 mb-1">
+                      <h6 className="text-base text-gray-300">NodeJs</h6>
+                      <p className="text-lg text-gray-400">80%</p>
+                    </div>
+                    <div className="progress w-full h-auto bg-slate-400 rounded-full">
+                      <div className="indicator bg-green-600 w-[80%] h-[0.35rem] rounded-l-full"></div>
+                    </div>
+                  </div>
+                  <div className="mb-4">
+                    <div className="flex items-center justify-between pr-2 mb-1">
+                      <h6 className="text-base text-gray-300">Django</h6>
+                      <p className="text-lg text-gray-400">92%</p>
+                    </div>
+                    <div className="progress w-full h-auto bg-slate-400 rounded-full">
+                      <div className="indicator bg-green-600 w-[92%] h-[0.35rem] rounded-l-full"></div>
+                    </div>
                   </div>
                 </div>
                 <div className="w-1/2 bg-slate-800 p-2 rounded-md">
