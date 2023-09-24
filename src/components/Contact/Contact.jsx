89ac221d6627e9515ff336a254239879149970c7
gokarna123-goku <agokarna444@gmail.com>
2023-09-24 15:18:10 +0545

    working on the form section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { UpcScan } from "react-bootstrap-icons";
+import { PersonFill, EnvelopePaperFill } from "react-bootstrap-icons";
 
 const Contact = () => {
   return (
@@ -9,16 +9,28 @@ const Contact = () => {
       </h1>
       <div className="w-full h-auto flex items-start justify-between gap-10">
         <div className="w-[40%] h-auto bg-green-600/10">Hello data</div>
-        <div className="w-[50%] h-auto">
-          <div className="flex items-center bg-slate-700/40 rounded w-full h-[42px] overflow-hidden">
-            <div className="w-[8%] h-full p-2 bg-green-400/10">
-                <UpcScan className="w-full h-full"/>
+        <div className="w-[55%] h-auto">
+          <div className="w-full h-full flex items-center gap-8">
+            <div className="flex items-center bg-slate-700/40 rounded w-full h-[45px] overflow-hidden mb-6">
+              <div className="w-[14%] h-full p-2 bg-green-400/10">
+                <PersonFill className="w-full h-full" />
+              </div>
+              <input
+                type="text"
+                className="w-[85%] h-full bg-transparent border-none outline-none px-4 text-base text-gray-500 placeholder:text-gray-500"
+                placeholder="Enter your fullname"
+              />
             </div>
-            <input
-              type="text"
-              className="w-[90%] h-full bg-transparent border-none outline-none px-4 text-base text-gray-500 placeholder:text-gray-500"
-              placeholder="Enter your fullname"
-            />
+            <div className="flex items-center bg-slate-700/40 rounded w-full h-[45px] overflow-hidden mb-6">
+              <div className="w-[14%] h-full p-2 bg-green-400/10">
+                <EnvelopePaperFill className="w-full h-full" />
+              </div>
+              <input
+                type="text"
+                className="w-[85%] h-full bg-transparent border-none outline-none px-4 text-base text-gray-500 placeholder:text-gray-500"
+                placeholder="Enter your fullname"
+              />
+            </div>
           </div>
         </div>
       </div>
