95db15c5c33b93f2ec2be1bf6df7ae61b8e7b169
gokarna123-goku <agokarna444@gmail.com>
2023-09-24 08:47:37 +0545

    changes the things

--- src/components/Service/SingleService/SingleService.jsx
+++ src/components/Service/SingleService/SingleService.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ArrowRight } from "react-bootstrap-icons";
+import Web from "../../../assets/web.png";
 
 const serviceData = {
   data: {
@@ -12,19 +13,29 @@ const serviceData = {
 
 const SingleService = () => {
   return (
-    <div className="flex items-start justify-center gap-8">
-    <div className="w-1/3 flex items-center justify-center flex-col bg-slate-900/60 hover:bg-gray-950/20 ease-out duration-700 shadow rounded py-5 px-4">
-        <div className="w-[60px] h-[60px] flex items-center justify-center gap-1 bg-green-500/40 rounded mb-5">
-            <ArrowRight />
+    <div className="flex items-start justify-center gap-12 flex-wrap">
+      <div className="w-[30%] flex items-center justify-center flex-col bg-slate-900/60 hover:bg-gray-950/20 ease-out duration-700 shadow rounded py-12 px-8">
+        <div className="w-[60px] h-[60px] flex items-center justify-center gap-1 bg-green-500/40 rounded mb-6">
+          <img
+            src={Web}
+            alt="Web Development"
+            className="w-[50px] h-[50px] object-cover object-center"
+          />
         </div>
+        <h1 className="text-gray-300 text-2xl font-bold tracking-wider mb-3">
+          Website Development
+        </h1>
+        <p className="text-base text-gray-500 text-center">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque
+          incidunt voluptatum consectetur iste! Ea, accusantium...
+        </p>
+      </div>
+      <div className="w-[30%] flex items-center justify-center flex-col bg-slate-900/60 hover:bg-gray-950/20 ease-out duration-700 shadow rounded py-12 px-4">
         <h1 className="text-slate-300 text-2xl font-bold">Hello Services</h1>
-    </div>
-    <div className="w-1/3 flex items-center justify-center flex-col bg-slate-900/60 hover:bg-gray-950/20 ease-out duration-700 shadow rounded py-5 px-4">
+      </div>
+      <div className="w-[30%] flex items-center justify-center flex-col bg-slate-900/60 hover:bg-gray-950/20 ease-out duration-700 shadow rounded py-12 px-4">
         <h1 className="text-slate-300 text-2xl font-bold">Hello Services</h1>
-    </div>
-    <div className="w-1/3 flex items-center justify-center flex-col bg-slate-900/60 hover:bg-gray-950/20 ease-out duration-700 shadow rounded py-5 px-4">
-        <h1 className="text-slate-300 text-2xl font-bold">Hello Services</h1>
-    </div>
+      </div>
     </div>
   );
 };
