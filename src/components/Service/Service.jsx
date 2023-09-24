6ab6882da94deee83f819491310fae49dd7c7fed
gokarna123-goku <agokarna444@gmail.com>
2023-09-24 07:44:30 +0545

    working on the dynamic service datas

--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import SingleService from "./SingleService/SingleService";
 
 const Service = () => {
   return (
     <>
-      <div className="p-32">
-        <h1>This is Service Section.</h1>
+      <div className="px-32 py-16">
+        <h1 className="text-slate-100 text-5xl text-center font-extrabold tracking-wider mb-12">
+          My Services
+        </h1>
+        <div className="w-full h-auto">
+            <SingleService />
+        </div>
       </div>
     </>
   );
---
+++ src/components/Service/SingleService/SingleService.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const serviceData = {
+  data: {
+    id: 1,
+    name: "Web Development",
+    img: "",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, eveniet assumenda atque vitae dolorem numquam.",
+  },
+};
+
+const SingleService = () => {
+  return (
+    <div>
+      <h1 className="text-slate-300">This is Single Service Section.</h1>
+    </div>
+  );
+};
+
+export default SingleService;
