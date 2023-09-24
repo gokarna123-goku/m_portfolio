23baa5e71d2d22101be2bb769d5f2f7a54364283
gokarna123-goku <agokarna444@gmail.com>
2023-09-24 12:46:29 +0545

    working on the blog section

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -4,7 +4,9 @@ const Blog = () => {
   return (
     <>
       <div className="px-32 p-16 bg-slate-950/20">
-        <h1 className="text-gray-300">This is Blog page.</h1>
+      <h1 className="text-slate-100 text-5xl text-center font-extrabold tracking-wider mb-12">
+          Recent Blog
+        </h1>
       </div>
     </>
   );
