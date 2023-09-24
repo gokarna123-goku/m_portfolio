eb20da4c91ff9aa800a41d21b7678304d2f5f4c5
gokarna123-goku <agokarna444@gmail.com>
2023-09-24 10:37:31 +0545

    adding the transition in the image section

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -9,7 +9,45 @@ const Portfolio = () => {
         <h1 className="text-slate-100 text-5xl text-center font-extrabold tracking-wider mb-12">
           Our Works
         </h1>
-        <div className="w-full h-auto">
+        <div className="w-full h-auto flex items-start justify-between flex-wrap gap-12">
+          <div className="w-[30%] flex items-start flex-col shadow-md shadow-gray-900 hover:shadow-gray-900/60 border-2 border-gray-700/30 ease-out duration-700 rounded-md p-4">
+            <div className="w-full h-[30vh] bg-green-900/20 rounded-md mb-5 overflow-hidden">
+              <img
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
+                alt="Portfolio Image"
+                className="w-full h-full object-cover object-center hover:scale-125 ease-out duration-700"
+              />
+            </div>
+            <div className="w-full h-auto flex items-center justify-between mb-5 px-1">
+              <h6 className="text-green-600 text-sm">web development</h6>
+              <div className="flex items-center gap-1 cursor-pointer">
+                <Eye className="text-gray-300 text-base" />
+                <p className="text-gray-300 text-sm">10k</p>
+              </div>
+            </div>
+            <Link className="text-gray-100/60 text-2xl font-medium tracking-wide mb-1">
+              Multi-Vendor Shoes Store Website
+            </Link>
+          </div>
+          <div className="w-[30%] flex items-start flex-col shadow-md shadow-gray-900 hover:shadow-gray-900/60 border-2 border-gray-700/30 ease-out duration-700 rounded-md p-4">
+            <div className="w-full h-[30vh] bg-green-900/20 rounded-md mb-5 overflow-hidden">
+              <img
+                src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
+                alt="Portfolio Image"
+                className="w-full h-full object-cover object-center"
+              />
+            </div>
+            <div className="w-full h-auto flex items-center justify-between mb-5 px-1">
+              <h6 className="text-green-600 text-sm">web development</h6>
+              <div className="flex items-center gap-1 cursor-pointer">
+                <Eye className="text-gray-300 text-base" />
+                <p className="text-gray-300 text-sm">10k</p>
+              </div>
+            </div>
+            <Link className="text-gray-100/60 text-2xl font-medium tracking-wide mb-1">
+              Multi-Vendor Shoes Store Website
+            </Link>
+          </div>
           <div className="w-[30%] flex items-start flex-col shadow-md shadow-gray-900 hover:shadow-gray-900/60 border-2 border-gray-700/30 ease-out duration-700 rounded-md p-4">
             <div className="w-full h-[30vh] bg-green-900/20 rounded-md mb-5 overflow-hidden">
               <img
