444e38cf30370f51eab39feb911a7c8290b6d35e
gokarna123-goku <agokarna444@gmail.com>
2023-09-24 15:37:01 +0545

    added the form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { PersonFill, EnvelopePaperFill } from "react-bootstrap-icons";
+import {
+  PersonFill,
+  EnvelopePaperFill,
+  TelephoneFill,
+  ChatDotsFill,
+} from "react-bootstrap-icons";
 
 const Contact = () => {
   return (
@@ -9,29 +14,50 @@ const Contact = () => {
       </h1>
       <div className="w-full h-auto flex items-start justify-between gap-10">
         <div className="w-[40%] h-auto bg-green-600/10">Hello data</div>
-        <div className="w-[50%] h-auto bg-green-700/20 p-5 rounded-md shadow">
-          <h2 className="text-gray-300 text-2xl font-medium mb-4">Write a message</h2>
-          <div className="w-full h-full flex items-center gap-8 mb-1">
-            <div className="flex items-center bg-slate-700/40 rounded w-full h-[45px] overflow-hidden mb-6">
-              <div className="w-[14%] h-full p-2 bg-green-400/10">
-                <PersonFill className="w-full h-full" />
-              </div>
-              <input
-                type="text"
-                className="w-[85%] h-full bg-transparent border-none outline-none px-4 text-base text-gray-500 placeholder:text-gray-500"
-                placeholder="Enter your fullname"
-              />
+        <div className="w-[50%] h-auto bg-green-700/10 py-6 px-8 rounded-md shadow">
+          <h2 className="text-gray-300 text-2xl font-medium mb-5">
+            Write a message
+          </h2>
+          <div className="flex items-center bg-slate-700/40 rounded w-full h-[50px] overflow-hidden mb-6">
+            <div className="w-[8%] h-full p-3 bg-green-400/10">
+              <PersonFill className="w-full h-full text-gray-900 text-[0.6rem]" />
             </div>
-            <div className="flex items-center bg-slate-700/40 rounded w-full h-[45px] overflow-hidden mb-6">
-              <div className="w-[14%] h-full p-2 bg-green-400/10">
-                <EnvelopePaperFill className="w-full h-full" />
-              </div>
-              <input
-                type="text"
-                className="w-[85%] h-full bg-transparent border-none outline-none px-4 text-base text-gray-500 placeholder:text-gray-500"
-                placeholder="Enter your email id"
-              />
+            <input
+              type="text"
+              className="w-[90%] h-full bg-transparent border-none outline-none px-4 text-base text-gray-500 placeholder:text-gray-500"
+              placeholder="Enter your fullname"
+            />
+          </div>
+          <div className="flex items-center bg-slate-700/40 rounded w-full h-[50px] overflow-hidden mb-6">
+            <div className="w-[8%] h-full p-3 bg-green-400/10">
+              <EnvelopePaperFill className="w-full h-full text-gray-900" />
             </div>
+            <input
+              type="text"
+              className="w-[85%] h-full bg-transparent border-none outline-none px-4 text-base text-gray-500 placeholder:text-gray-500"
+              placeholder="Enter your email id"
+            />
+          </div>
+          <div className="flex items-center bg-slate-700/40 rounded w-full h-[50px] overflow-hidden mb-6">
+            <div className="w-[8%] h-full p-3 bg-green-400/10">
+              <TelephoneFill className="w-full h-full text-gray-900 text-sm" />
+            </div>
+            <input
+              type="text"
+              className="w-[85%] h-full bg-transparent border-none outline-none px-4 text-base text-gray-500 placeholder:text-gray-500"
+              placeholder="Enter your email id"
+            />
+          </div>
+          <div className="flex items-start bg-slate-700/40 rounded w-full h-[120px] overflow-hidden mb-6">
+            <div className="w-[8%] h-auto p-3 bg-green-400/10">
+              <ChatDotsFill className="w-full h-full text-gray-900 text-sm" />
+            </div>
+            <textarea
+              cols="30"
+              rows="4"
+              className="w-[90%] h-full outline-none border-none bg-transparent p-4 text-base text-gray-500 placeholder:text-gray-500 resize-none"
+              placeholder="Type a message..."
+            ></textarea>
           </div>
         </div>
       </div>
