63306d208458691b183eaa42372c58ab1ea44692
gokarna123-goku <agokarna444@gmail.com>
2023-09-24 12:24:34 +0545

    working on the portfolio loading section

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Eye, ArrowRight } from "react-bootstrap-icons";
+import SinglePortfolio from "./SinglePortfolio/SinglePortfolio";
 
 const Portfolio = () => {
   return (
@@ -9,73 +8,10 @@ const Portfolio = () => {
         <h1 className="text-slate-100 text-5xl text-center font-extrabold tracking-wider mb-12">
           Our Works
         </h1>
-        <div className="w-full h-auto flex items-start justify-center flex-wrap gap-10">
-          <div className="w-[31%] flex items-start flex-col shadow-md shadow-gray-900 hover:shadow-gray-900/60 border-2 border-gray-700/30 ease-out duration-700 rounded-md p-4">
-            <div className="w-full h-[30vh] bg-green-900/20 rounded-md mb-5 overflow-hidden">
-              <img
-                src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
-                alt="Portfolio Image"
-                className="w-full h-full object-cover object-center hover:rotate-6 hover:scale-125 ease-out duration-700"
-              />
-            </div>
-            <div className="w-full h-auto flex items-center justify-between mb-5 px-1">
-              <h6 className="text-green-600 text-sm">web development</h6>
-              <div className="flex items-center gap-1 cursor-pointer">
-                <Eye className="text-gray-300 text-base" />
-                <p className="text-gray-300 text-sm">10k</p>
-              </div>
-            </div>
-            <h1 className="text-gray-100/60 text-2xl font-medium tracking-wide mb-4">
-              Multi-Vendor Shoes Store Website
-            </h1>
-            <Link className="text-green-400 text-[1.1rem] flex items-center gap-1 mb-3">
-              Read more <ArrowRight />
-            </Link>
-          </div>
-          <div className="w-[31%] flex items-start flex-col shadow-md shadow-gray-900 hover:shadow-gray-900/60 border-2 border-gray-700/30 ease-out duration-700 rounded-md p-4">
-            <div className="w-full h-[30vh] bg-green-900/20 rounded-md mb-5 overflow-hidden">
-              <img
-                src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg"
-                alt="Portfolio Image"
-                className="w-full h-full object-cover object-center hover:rotate-6 hover:scale-125 ease-out duration-700"
-              />
-            </div>
-            <div className="w-full h-auto flex items-center justify-between mb-5 px-1">
-              <h6 className="text-green-600 text-sm">web development</h6>
-              <div className="flex items-center gap-1 cursor-pointer">
-                <Eye className="text-gray-300 text-base" />
-                <p className="text-gray-300 text-sm">10k</p>
-              </div>
-            </div>
-            <h1 className="text-gray-100/60 text-2xl font-medium tracking-wide mb-4">
-              PS5 Gaming Controller Store Website
-            </h1>
-            <Link className="text-green-400 text-[1.1rem] flex items-center gap-1 mb-3">
-              Read more <ArrowRight />
-            </Link>
-          </div>
-          <div className="w-[31%] flex items-start flex-col shadow-md shadow-gray-900 hover:shadow-gray-900/60 border-2 border-gray-700/30 ease-out duration-700 rounded-md p-4">
-            <div className="w-full h-[30vh] bg-green-900/20 rounded-md mb-5 overflow-hidden">
-              <img
-                src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
-                alt="Portfolio Image"
-                className="w-full h-full object-cover object-center hover:rotate-6 hover:scale-125 ease-out duration-700"
-              />
-            </div>
-            <div className="w-full h-auto flex items-center justify-between mb-5 px-1">
-              <h6 className="text-green-600 text-sm">web development</h6>
-              <div className="flex items-center gap-1 cursor-pointer">
-                <Eye className="text-gray-300 text-base" />
-                <p className="text-gray-300 text-sm">10k</p>
-              </div>
-            </div>
-            <h1 className="text-gray-100/60 text-2xl font-medium tracking-wide mb-4">
-              Multi-Vendor Shoes Store Website
-            </h1>
-            <Link className="text-green-400 text-[1.1rem] flex items-center gap-1 mb-3">
-              Read more <ArrowRight />
-            </Link>
-          </div>
+        <SinglePortfolio />
+        <SinglePortfolio />
+        <div className="flex items-center justify-center">
+        <button className="bg-transparent hover:bg-green-500/10 border-2 border-green-600 ease-out duration-700 px-7 py-2 rounded text-lg text-gray-300">Load more</button>
         </div>
       </div>
     </>
---
+++ src/components/Portfolio/SinglePortfolio/SinglePortfolio.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Eye, ArrowRight } from "react-bootstrap-icons";
+
+const SinglePortfolio = () => {
+  return (
+    <div className="w-full h-auto flex items-start justify-center flex-wrap gap-10 mb-10">
+      <div className="w-[31%] flex items-start flex-col shadow-md shadow-gray-900 hover:shadow-gray-900/60 border-2 border-gray-700/30 ease-out duration-700 rounded-md p-4">
+        <div className="w-full h-[30vh] bg-green-900/20 rounded-md mb-5 overflow-hidden">
+          <img
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
+            alt="Portfolio Image"
+            className="w-full h-full object-cover object-center hover:rotate-6 hover:scale-125 ease-out duration-700"
+          />
+        </div>
+        <div className="w-full h-auto flex items-center justify-between mb-5 px-1">
+          <h6 className="text-green-600 text-sm">web development</h6>
+          <div className="flex items-center gap-1 cursor-pointer">
+            <Eye className="text-gray-300 text-base" />
+            <p className="text-gray-300 text-sm">10k</p>
+          </div>
+        </div>
+        <h1 className="text-gray-100/60 text-2xl font-medium tracking-wide mb-4">
+          Multi-Vendor Shoes Store Website
+        </h1>
+        <Link className="text-green-400 text-[1.1rem] flex items-center gap-1 mb-3">
+          Read more <ArrowRight />
+        </Link>
+      </div>
+      <div className="w-[31%] flex items-start flex-col shadow-md shadow-gray-900 hover:shadow-gray-900/60 border-2 border-gray-700/30 ease-out duration-700 rounded-md p-4">
+        <div className="w-full h-[30vh] bg-green-900/20 rounded-md mb-5 overflow-hidden">
+          <img
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg"
+            alt="Portfolio Image"
+            className="w-full h-full object-cover object-center hover:rotate-6 hover:scale-125 ease-out duration-700"
+          />
+        </div>
+        <div className="w-full h-auto flex items-center justify-between mb-5 px-1">
+          <h6 className="text-green-600 text-sm">web development</h6>
+          <div className="flex items-center gap-1 cursor-pointer">
+            <Eye className="text-gray-300 text-base" />
+            <p className="text-gray-300 text-sm">10k</p>
+          </div>
+        </div>
+        <h1 className="text-gray-100/60 text-2xl font-medium tracking-wide mb-4">
+          PS5 Gaming Controller Store Website
+        </h1>
+        <Link className="text-green-400 text-[1.1rem] flex items-center gap-1 mb-3">
+          Read more <ArrowRight />
+        </Link>
+      </div>
+      <div className="w-[31%] flex items-start flex-col shadow-md shadow-gray-900 hover:shadow-gray-900/60 border-2 border-gray-700/30 ease-out duration-700 rounded-md p-4">
+        <div className="w-full h-[30vh] bg-green-900/20 rounded-md mb-5 overflow-hidden">
+          <img
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
+            alt="Portfolio Image"
+            className="w-full h-full object-cover object-center hover:rotate-6 hover:scale-125 ease-out duration-700"
+          />
+        </div>
+        <div className="w-full h-auto flex items-center justify-between mb-5 px-1">
+          <h6 className="text-green-600 text-sm">web development</h6>
+          <div className="flex items-center gap-1 cursor-pointer">
+            <Eye className="text-gray-300 text-base" />
+            <p className="text-gray-300 text-sm">10k</p>
+          </div>
+        </div>
+        <h1 className="text-gray-100/60 text-2xl font-medium tracking-wide mb-4">
+          Multi-Vendor Shoes Store Website
+        </h1>
+        <Link className="text-green-400 text-[1.1rem] flex items-center gap-1 mb-3">
+          Read more <ArrowRight />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default SinglePortfolio;
