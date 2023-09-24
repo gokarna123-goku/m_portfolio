1fd6b9702da9c42dfb888810960e52e5d1c8aaa2
gokarna123-goku <agokarna444@gmail.com>
2023-09-24 15:22:48 +0545

    making it form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -9,8 +9,9 @@ const Contact = () => {
       </h1>
       <div className="w-full h-auto flex items-start justify-between gap-10">
         <div className="w-[40%] h-auto bg-green-600/10">Hello data</div>
-        <div className="w-[55%] h-auto">
-          <div className="w-full h-full flex items-center gap-8">
+        <div className="w-[50%] h-auto bg-green-700/20 p-5 rounded-md shadow">
+          <h2 className="text-gray-300 text-2xl font-medium mb-4">Write a message</h2>
+          <div className="w-full h-full flex items-center gap-8 mb-1">
             <div className="flex items-center bg-slate-700/40 rounded w-full h-[45px] overflow-hidden mb-6">
               <div className="w-[14%] h-full p-2 bg-green-400/10">
                 <PersonFill className="w-full h-full" />
@@ -28,7 +29,7 @@ const Contact = () => {
               <input
                 type="text"
                 className="w-[85%] h-full bg-transparent border-none outline-none px-4 text-base text-gray-500 placeholder:text-gray-500"
-                placeholder="Enter your fullname"
+                placeholder="Enter your email id"
               />
             </div>
           </div>
