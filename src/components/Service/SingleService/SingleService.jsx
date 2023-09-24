212f57e75ad7621b4b7e90a18a5aad646cd0d3c6
gokarna123-goku <agokarna444@gmail.com>
2023-09-24 08:15:58 +0545

    i will be back

--- src/components/Service/SingleService/SingleService.jsx
+++ src/components/Service/SingleService/SingleService.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ArrowRight } from "react-bootstrap-icons";
 
 const serviceData = {
   data: {
@@ -13,6 +14,9 @@ const SingleService = () => {
   return (
     <div className="flex items-start justify-center gap-8">
     <div className="w-1/3 flex items-center justify-center flex-col bg-slate-900/60 hover:bg-gray-950/20 ease-out duration-700 shadow rounded py-5 px-4">
+        <div className="w-[60px] h-[60px] flex items-center justify-center gap-1 bg-green-500/40 rounded mb-5">
+            <ArrowRight />
+        </div>
         <h1 className="text-slate-300 text-2xl font-bold">Hello Services</h1>
     </div>
     <div className="w-1/3 flex items-center justify-center flex-col bg-slate-900/60 hover:bg-gray-950/20 ease-out duration-700 shadow rounded py-5 px-4">
