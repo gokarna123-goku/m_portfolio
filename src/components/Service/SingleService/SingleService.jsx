275a1b2ebe3246d854a03b6cb3ff085e9916ccff
gokarna123-goku <agokarna444@gmail.com>
2023-09-24 08:57:35 +0545

    completed the service section

--- src/components/Service/SingleService/SingleService.jsx
+++ src/components/Service/SingleService/SingleService.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { ArrowRight } from "react-bootstrap-icons";
 import Web from "../../../assets/web.png";
+import App from "../../../assets/app.png";
+import UxUi from "../../../assets/design.png";
 
 const serviceData = {
   data: {
@@ -23,7 +25,7 @@ const SingleService = () => {
             className="w-[50px] h-[50px] object-cover object-center"
           />
         </div>
-        <h1 className="text-gray-300 text-2xl tracking-widest mb-3">
+        <h1 className="text-gray-300 text-2xl font-medium tracking-wider mb-3">
           Website Development
         </h1>
         <p className="text-base text-gray-500 text-center mb-5">
@@ -32,11 +34,39 @@ const SingleService = () => {
         </p>
         <Link className="text-green-400 text-[1.1rem] flex items-center gap-1">Read more <ArrowRight /></Link>
       </div>
-      <div className="w-[30%] flex items-center justify-center flex-col bg-slate-900/60 hover:bg-gray-950/20 ease-out duration-700 shadow rounded py-12 px-4">
-        <h1 className="text-slate-300 text-2xl font-bold">Hello Services</h1>
+      <div className="w-[30%] flex items-center justify-center flex-col bg-slate-900/60 hover:bg-gray-950/20 ease-out duration-700 shadow rounded py-12 px-8">
+        <div className="w-[60px] h-[60px] flex items-center justify-center gap-1 bg-green-500/40 rounded mb-6">
+          <img
+            src={App}
+            alt="App Development"
+            className="w-[50px] h-[50px] object-cover object-center"
+          />
+        </div>
+        <h1 className="text-gray-300 text-2xl font-medium tracking-wider mb-3">
+          Mobile App Development
+        </h1>
+        <p className="text-base text-gray-500 text-center mb-5">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque
+          incidunt voluptatum consectetur iste! Ea, accusantium...
+        </p>
+        <Link className="text-green-400 text-[1.1rem] flex items-center gap-1">Read more <ArrowRight /></Link>
       </div>
-      <div className="w-[30%] flex items-center justify-center flex-col bg-slate-900/60 hover:bg-gray-950/20 ease-out duration-700 shadow rounded py-12 px-4">
-        <h1 className="text-slate-300 text-2xl font-bold">Hello Services</h1>
+      <div className="w-[30%] flex items-center justify-center flex-col bg-slate-900/60 hover:bg-gray-950/20 ease-out duration-700 shadow rounded py-12 px-8">
+        <div className="w-[60px] h-[60px] flex items-center justify-center gap-1 bg-green-500/40 rounded mb-6">
+          <img
+            src={UxUi}
+            alt="Ux/Ui Design"
+            className="w-[50px] h-[50px] object-cover object-center"
+          />
+        </div>
+        <h1 className="text-gray-300 text-2xl font-medium tracking-wider mb-3">
+          Ux/Ui Design
+        </h1>
+        <p className="text-base text-gray-500 text-center mb-5">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque
+          incidunt voluptatum consectetur iste! Ea, accusantium...
+        </p>
+        <Link className="text-green-400 text-[1.1rem] flex items-center gap-1">Read more <ArrowRight /></Link>
       </div>
     </div>
   );
