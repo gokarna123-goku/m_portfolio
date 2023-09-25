0aaff9d6c0b39f30ef8870004a64bd4b783bab8e
gokarna123-goku <agokarna444@gmail.com>
2023-09-25 09:58:12 +0545

    completed the portfolio responsive section

--- src/components/Portfolio/SinglePortfolio/SinglePortfolio.jsx
+++ src/components/Portfolio/SinglePortfolio/SinglePortfolio.jsx
@@ -5,8 +5,8 @@ import { Eye, ArrowRight } from "react-bootstrap-icons";
 const SinglePortfolio = () => {
   return (
     <div className="w-full h-auto flex items-start justify-center flex-wrap gap-10 mb-10">
-      <div className="w-[31%] flex items-start flex-col shadow-md shadow-gray-900 hover:shadow-gray-900/60 border-2 border-gray-700/30 ease-out duration-700 rounded-md p-4">
-        <div className="w-full h-[30vh] bg-green-900/20 rounded-md mb-5 overflow-hidden">
+      <div className="lg:w-[31%] md:w-[31%] sm:w-full w-full flex items-start flex-col shadow-md shadow-gray-900 hover:shadow-gray-900/60 border-2 border-gray-700/30 ease-out duration-700 rounded-md p-4">
+        <div className="w-full lg:h-[30vh] md:h-[28vh] sm:h-[23vh] h-[23vh] bg-green-900/20 rounded-md mb-5 overflow-hidden">
           <img
             src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
             alt="Portfolio Image"
@@ -20,15 +20,15 @@ const SinglePortfolio = () => {
             <p className="text-gray-300 text-sm">10k</p>
           </div>
         </div>
-        <h1 className="text-gray-100/60 text-2xl font-medium tracking-wide mb-4">
+        <h1 className="text-gray-100/60 lg:text-2xl md:text-2xl sm:text-[1.3rem] text-[1.3rem] font-medium tracking-wide mb-4">
           Multi-Vendor Shoes Store Website
         </h1>
         <Link className="text-green-400 text-[1.1rem] flex items-center gap-1 mb-3">
           Read more <ArrowRight />
         </Link>
       </div>
-      <div className="w-[31%] flex items-start flex-col shadow-md shadow-gray-900 hover:shadow-gray-900/60 border-2 border-gray-700/30 ease-out duration-700 rounded-md p-4">
-        <div className="w-full h-[30vh] bg-green-900/20 rounded-md mb-5 overflow-hidden">
+      <div className="lg:w-[31%] md:w-[31%] sm:w-full w-full flex items-start flex-col shadow-md shadow-gray-900 hover:shadow-gray-900/60 border-2 border-gray-700/30 ease-out duration-700 rounded-md p-4">
+        <div className="w-full lg:h-[30vh] md:h-[28vh] sm:h-[25vh] h-[25vh] bg-green-900/20 rounded-md mb-5 overflow-hidden">
           <img
             src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg"
             alt="Portfolio Image"
@@ -42,15 +42,15 @@ const SinglePortfolio = () => {
             <p className="text-gray-300 text-sm">10k</p>
           </div>
         </div>
-        <h1 className="text-gray-100/60 text-2xl font-medium tracking-wide mb-4">
+        <h1 className="text-gray-100/60 lg:text-2xl md:text-2xl sm:text-[1.3rem] text-[1.3rem] font-medium tracking-wide mb-4">
           PS5 Gaming Controller Store Website
         </h1>
         <Link className="text-green-400 text-[1.1rem] flex items-center gap-1 mb-3">
           Read more <ArrowRight />
         </Link>
       </div>
-      <div className="w-[31%] flex items-start flex-col shadow-md shadow-gray-900 hover:shadow-gray-900/60 border-2 border-gray-700/30 ease-out duration-700 rounded-md p-4">
-        <div className="w-full h-[30vh] bg-green-900/20 rounded-md mb-5 overflow-hidden">
+      <div className="lg:w-[31%] md:w-[31%] sm:w-full w-full flex items-start flex-col shadow-md shadow-gray-900 hover:shadow-gray-900/60 border-2 border-gray-700/30 ease-out duration-700 rounded-md p-4">
+        <div className="w-full lg:h-[30vh] md:h-[28vh] sm:h-[25vh] h-[25vh] bg-green-900/20 rounded-md mb-5 overflow-hidden">
           <img
             src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
             alt="Portfolio Image"
@@ -64,7 +64,7 @@ const SinglePortfolio = () => {
             <p className="text-gray-300 text-sm">10k</p>
           </div>
         </div>
-        <h1 className="text-gray-100/60 text-2xl font-medium tracking-wide mb-4">
+        <h1 className="text-gray-100/60 lg:text-2xl md:text-2xl sm:text-[1.3rem] text-[1.3rem] font-medium tracking-wide mb-4">
           Multi-Vendor Shoes Store Website
         </h1>
         <Link className="text-green-400 text-[1.1rem] flex items-center gap-1 mb-3">
