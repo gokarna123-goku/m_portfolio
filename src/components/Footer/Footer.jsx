0c0c683a7ef40a791fb3f955ba2820386b3cba37
gokarna123-goku <agokarna444@gmail.com>
2023-09-25 10:33:43 +0545

    working on the section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -20,11 +20,11 @@ const Footer = () => {
           <h1 className="text-slate-400  lg:text-5xl md:text-5xl sm:text-4xl text-4xl font-extrabold tracking-widest lg:mr-16 md:mr-15 sm:mr-0 mr-0 lg:mb-0 md:mb-1 sm:mb-10 mb-10">
             Have a project?
           </h1>
-          <button className="bg-green-700 text-lg text-slate-300 hover:bg-green-800 hover:text-slate-200 duration-700 ease-out px-6 py-2 rounded-md ml-16 flex items-center">
+          <button className="bg-green-700 text-lg text-slate-300 hover:bg-green-800 hover:text-slate-200 duration-700 ease-out px-6 py-2 rounded-md lg:ml-16 md:ml-15 sm:ml-0 ml-0 flex items-center">
             Let's talk <ArrowRight className="ml-2" />
           </button>
         </div>
-        <div className="flex items-center justify-center gap-5 my-16">
+        <div className="flex items-center justify-center gap-5 my-16 flex-wrap">
           <Link className="p-3 rounded bg-slate-900 hover:shadow-sm text-2xl"> <Github className="text-2xl text-slate-500 hover:text-green-500 duration-700 ease-out" /> </Link>
           <Link className="p-3 rounded bg-slate-900 hover:shadow-sm text-2xl"> <Instagram className="text-2xl text-slate-500 hover:text-green-500 duration-700 ease-out" /> </Link>
           <Link className="p-3 rounded bg-slate-900 hover:shadow-sm text-2xl"> <Youtube className="text-2xl text-slate-500 hover:text-green-500 duration-700 ease-out" /> </Link>
@@ -33,7 +33,7 @@ const Footer = () => {
           <Link className="p-3 rounded bg-slate-900 hover:shadow-sm text-2xl"> <Twitter className="text-2xl text-slate-500 hover:text-green-500 duration-700 ease-out" /> </Link>
           <Link className="p-3 rounded bg-slate-900 hover:shadow-sm text-2xl"> <Twitch className="text-2xl text-slate-500 hover:text-green-500 duration-700 ease-out" /> </Link>
         </div>
-        <div className="divider my-7 w-full h-[0.6px]"></div>
+        <div className="divider lg:my-7 md:my-6 sm:my-4 my-3 w-full h-[0.6px]"></div>
         <div className="flex items-center justify-center text-slate-700 font-2xl font-normal">
             Copyright &copy; 2023 Goku. All right reserved!
         </div>
