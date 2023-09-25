79faff3b1953a82c89c0876a87ac014fe37956a7
gokarna123-goku <agokarna444@gmail.com>
2023-09-25 08:40:56 +0545

    done with hero section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,12 +8,12 @@ const Home = () => {
     <>
       <div className="hero_img lg:px-32 md:px-16 sm:px-10 px-10 relative">
         {/* <div className="animate-ping h-16 w-16 rounded-full absolute top-80 left-10 bg-sky-400 duration-1000 ease-out delay-1000 opacity-5"></div> */}
-        <div className="lg:w-2/4 md:w-2/3 sm:w-full mt-8">
+        <div className="lg:w-2/4 md:w-2/3 sm:w-full lg:mt-8 md:mt-5 sm:mt-1 mt-0">
           {/* <h6 className="text-slate-400 mb-2 text-xl">Hey 👋 There, It's</h6> */}
           <p className="text-gray-400  lg:text-2xl md:text-xl sm:text-xl text-xl font-medium tracking-[0.05rem] mb-4">
             Meet a Front~End Developer💻,
           </p>
-          <h1 className="text-slate-100 lg:text-8xl md:text-6xl sm:text-6xl text-5xl font-black lg:leading-[7.3rem] tracking-[0.12rem] mb-8">
+          <h1 className="text-slate-100 lg:text-8xl md:text-6xl sm:text-6xl text-5xl font-black lg:leading-[7.3rem] md:leading-[6rem] sm:leading-[4.2rem] leading-[4rem] tracking-[0.12rem] mb-8">
             Mr. <span className="text-green-400">Gokarna</span> Adhikari!
           </h1>
           <Link className="flex items-center text-xl text-green-500 text-left font-medium px-0 py-2 rounded-md hover:text-green-600 ease-out duration-1000">
