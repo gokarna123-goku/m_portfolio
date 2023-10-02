9809ea4abff0c90febbc94b67aefd811a1db6bae
gokarna123-goku <agokarna444@gmail.com>
2023-10-02 08:05:42 +0545

    almost done the navbar section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     <>
       <div className="w-full h-auto relative flex items-center justify-center">
         {/* For Smaller Devices */}
-        <nav className="w-1/3 bg-slate-950/30 backdrop-blur-[6px] py-6 px-10 shadow flex items-center justify-between fixed bottom-10 rounded-full">
+        <nav className="w-1/3 bg-slate-900/60 border border-green-400/10 backdrop-blur-[6px] py-5 px-10 shadow flex items-center justify-between fixed bottom-10 rounded-full">
           <ul className="w-full h-auto list-none flex items-center justify-between">
             <li>
               <Link
