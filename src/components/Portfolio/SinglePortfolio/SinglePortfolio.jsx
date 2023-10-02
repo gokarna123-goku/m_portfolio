64b5db8accc9747ef1527ce6f813d339886b848b
gokarna123-goku <agokarna444@gmail.com>
2023-10-02 10:16:33 +0545

    completed the navbar

--- src/components/Portfolio/SinglePortfolio/SinglePortfolio.jsx
+++ src/components/Portfolio/SinglePortfolio/SinglePortfolio.jsx
@@ -28,7 +28,7 @@ const SinglePortfolio = () => {
         </Link>
       </div>
       <div className="lg:w-[31%] md:w-full sm:w-full w-full flex items-start flex-col shadow-md shadow-gray-900 hover:shadow-gray-900/60 border-2 border-gray-700/30 ease-out duration-700 rounded-md p-4">
-        <div className="w-full lg:h-[30vh] md:h-[45vh] sm:h-[25vh] h-[25vh] bg-green-900/20 rounded-md mb-5 overflow-hidden">
+        <div className="w-full lg:h-[30vh] md:h-[45vh] sm:h-[40vh] h-[25vh] bg-green-900/20 rounded-md mb-5 overflow-hidden">
           <img
             src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg"
             alt="Portfolio Image"
@@ -50,7 +50,7 @@ const SinglePortfolio = () => {
         </Link>
       </div>
       <div className="lg:w-[31%] md:w-full sm:w-full w-full flex items-start flex-col shadow-md shadow-gray-900 hover:shadow-gray-900/60 border-2 border-gray-700/30 ease-out duration-700 rounded-md p-4">
-        <div className="w-full lg:h-[30vh] md:h-[45vh] sm:h-[25vh] h-[25vh] bg-green-900/20 rounded-md mb-5 overflow-hidden">
+        <div className="w-full lg:h-[30vh] md:h-[45vh] sm:h-[35vh] h-[25vh] bg-green-900/20 rounded-md mb-5 overflow-hidden">
           <img
             src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
             alt="Portfolio Image"
